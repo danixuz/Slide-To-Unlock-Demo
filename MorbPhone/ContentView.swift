d6778f66147ccf4acad4cff2648e92e9d1ee7f63
danixuz <danixuz@icloud.com>
2022-05-31 09:40:00 +0300

    Initial Commit

---
+++ MorbPhone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MorbPhone
+//
+//  Created by Daniel Spalek on 31/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MorbPhone/MorbPhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorbPhoneApp.swift
+//  MorbPhone
+//
+//  Created by Daniel Spalek on 31/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct MorbPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
